e2e68e6f135d06a74f3cd49354f4a011c5518b1e
James Hanlon <mail@jameswhanlon.com>
2023-04-21 09:25:55 +0100

    Improve FindLibElf script

--- cmake/FindLibElf.cmake
+++ cmake/FindLibElf.cmake
@@ -1,40 +1,34 @@
-# Attempt to find libelf.
+# Attempt to find libelf headers and library.
+# This module defines:
+#  LIBELF_FOUND
+#  LIBELF_INCLUDE_DIRS
+#  LIBELF_LIBRARIES
 
-find_path(LIBELF_INCLUDE_DIRS
-  NAMES
-    libelf.h
-  PATHS
-    /usr/include
-    /usr/include/libelf
-    /usr/local/include
-    /usr/local/include/libelf
-    /opt/local/include
-    /opt/local/include/libelf
-  ENV CPATH)
+find_path(LIBELF_INCLUDE_DIR
+          NAMES libelf.h
+          PATH_SUFFIXES libelf)
 
 # If libelf.h is found in a 'libelf/' directory, also include the parent level
 # as a search path since other ELF headers reside here on some platforms.
-if (LIBELF_INCLUDE_DIRS MATCHES "libelf$")
-  set(LIBELF_INCLUDE_DIRS "${LIBELF_INCLUDE_DIRS}"
-                          "${LIBELF_INCLUDE_DIRS}/.."
+if (LIBELF_INCLUDE_DIR MATCHES "libelf$")
+  string(REGEX REPLACE "libelf$" ""
+           LILBELF_INCLIDE_DIRS
+           "${LIBELF_INCLUDE_DIR}")
+  set(LIBELF_INCLUDE_DIRS
+       "${LIBELF_INCLUDE_DIRS}"
+       "${LIBELF_INCLUDE_DIR}"
       CACHE STRING "" FORCE)
+else()
+  set(LIBELF_INCLUDE_DIRS "${LIBELF_INCLUDE_DIR}")
 endif()
 
 find_library(LIBELF_LIBRARIES
-  NAMES
-    elf
-  PATHS
-    /usr/lib
-    /usr/local/lib
-    /opt/local/lib
-    /sw/lib
-  ENV LIBRARY_PATH
-  ENV LD_LIBRARY_PATH)
+             NAMES elf)
 
 include(FindPackageHandleStandardArgs)
 
 # handle the QUIETLY and REQUIRED arguments and set LIBELF_FOUND to TRUE if
 # all listed variables are TRUE.
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibElf DEFAULT_MSG
+find_package_handle_standard_args(LibElf DEFAULT_MSG
   LIBELF_LIBRARIES
   LIBELF_INCLUDE_DIRS)
