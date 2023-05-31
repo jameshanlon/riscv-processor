2d13d69eb46b3f965176140903d2b60f992c61b1
James Hanlon <mail@jameswhanlon.com>
2023-05-31 17:13:31 +0100

    Fixes for RISCV tools build script

--- build_riscv_tooling.sh
+++ build_riscv_tooling.sh
@@ -7,27 +7,19 @@ NUM_PARALLEL_JOBS=8
 mkdir -p $WORK_DIR
 mkdir -p $INSTALL_DIR
 
+git clone --depth=1 https://github.com/riscv-collab/riscv-gnu-toolchain.git $WORK_DIR/riscv-gnu-toolchain
+git clone --depth=1 https://github.com/riscv-software-src/riscv-isa-sim.git $WORK_DIR/riscv-isa-sim
+
 # riscv-gnu-toolchain
-echo "## riscv-gnu-toolchain"
-git clone --depth=1 https://github.com/riscv-collab/riscv-gnu-toolchain.git $WORK_DIR/
+echo "## Building riscv-gnu-toolchain"
 (cd $WORK_DIR/riscv-gnu-toolchain;
  ./configure --prefix=$INSTALL_DIR --with-arch=rv32im;
  make -j $NUM_PARALLEL_JOBS
 )
 
-# riscv-pk
-echo "## riscv-pk"
-git clone --depth=1 https://github.com/riscv-software-src/riscv-pk.git $WORK_DIR/
-(cd $WORK_DIR/riscv-pk;
- ./configure --prefix=$INSTALL_DIR --host=riscv32-unknown-elf;
- make -j $NUM_PARALLEL_JOBS;
- make install
-)
-
 # riscv-isa-sim
-echo "## riscv-isa-sim"
-git clone --depth=1 https://github.com/riscv-software-src/riscv-isa-sim.git $WORK_DIR/
-(cd $WORK/riscv-isa-sim;
+echo "## Building riscv-isa-sim"
+(cd $WORK_DIR/riscv-isa-sim;
  ./configure --prefix=$INSTALL_DIR --with-target=riscv32-unknown-elf
  make -j $NUM_PARALLEL_JOBS;
  make install
